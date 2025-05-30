ea2756e0375e8d6ee49c33f3fd03544647d40619
Iqraa Manuel <iqraathemanuel@icloud.com>
2025-05-30 17:56:41 -0400

    Initial Commit

---
+++ BetterTTS/BetterTTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterTTSApp.swift
+//  BetterTTS
+//
+//  Created by Iqraa Manuel on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterTTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
